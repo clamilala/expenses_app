b88e890650c8f8b9cc6a4c86fb1754a831934dc6
Shibata Satoru <clamilala@gmail.com>
2019-11-17 01:07:07 +0900

    [add]Users Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    #仮コード
+    redirect_to("/appsettings")
+  end
+
+    
+  def edit
+  end
+
+  def show
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get '/users/new' => 'users#new'
+  post '/users/' => 'users#create'
+  get '/users/edit' => 'users#edit'
+  get '/users/show'  => 'users#show'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   
