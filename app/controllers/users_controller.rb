39c56af53da3ef5e4290f04d86e2c7c999a64839
Shibata Satoru <clamilala@gmail.com>
2019-11-17 23:21:54 +0900

    [update]crate user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    
+    @user = User.new(
+      name: params[:name],
+      login_id: params[:login_id],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    
+    if @user.save
+      redirect_to("/", notice: "ユーザー「#{@user.name}」を登録しました。")
+    else
+      render(:new)
+    end
+    
     #仮コード
-    redirect_to("/appsettings")
+    #redirect_to("/appsettings")
   end
 
     
@@ -14,4 +29,6 @@ class UsersController < ApplicationController
   def show
   end
   
+  
+
 end
