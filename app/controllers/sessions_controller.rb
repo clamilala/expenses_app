12408ecd362e037d3bf1fd203912436aa70a0e1b
Shibata Satoru <clamilala@gmail.com>
2019-11-27 00:47:30 +0900

    [update]login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
   def new
-
+    
   end
   
   def create
-    #仮コード
-    redirect_to("/")
+    user = User.find_by(login_id: session_params[:login_id])
+    
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to "/", notice: 'ログインしました'
+    else
+      flash[:notice] = 'ログインに失敗しました'
+      render :new, notice: 'ログインに失敗しました'
+    end
   end
+  
+  private 
+    def session_params
+      params.require(:session).permit(:login_id, :password)
+    end
 end
